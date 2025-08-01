d531caf694a4f913bb5f1a784df7f7ec79c3ed67
Goat509-HD <mantiktoker7@gmail.com>
2025-07-31 23:25:22 -0400

    Update config.js

--- config.js
+++ config.js
@@ -13,14 +13,15 @@ module.exports = {
     CHATBOT: getConfig("CHATBOT") || "on",
     //chatbot on/off
   
-    SESSION_ID: process.env.SESSION_ID || "MEGALODON-MD~",  // Your bot's session ID (keep it secure)
-    PREFIX: getConfig("PREFIX") || ".",  // Command prefix (e.g., "., / ! * - +")
+    SESSION_ID: process.env.SESSION_ID || "MEGALODON~MD~flh2URCC#ETL2Nu4waPhcnmBEItFYWyu5w3CAXOzGezw4rOVCrVE",  // HM5LNEYD
+
+    PREFIX: getConfig("PREFIX") || "·",  // Command prefix (e.g., "., / ! * - +")
     BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "MEGALODON-MD",  // Bot's display name
-    MODE: process.env.MODE || "public",        // Bot mode: public/private/group/inbox
+    MODE: process.env.MODE || "private",        // Bot mode: public/private/group/inbox
     // ===== OWNER & DEVELOPER SETTINGS =====
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "50948702213",  // Owner's WhatsApp number
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "50946062382",  // Owner's WhatsApp number
     
-    OWNER_NAME: process.env.OWNER_NAME || getConfig("OWNER_NAME") || "",           // Owner's name
+    OWNER_NAME: process.env.OWNER_NAME || getConfig("HD") || "",           // Owner's name
     
     DEV: process.env.DEV || "50948336180",                     // Developer's contact number
     
